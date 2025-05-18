fc786582f140a359e16af72551b3ea1901159ca2
Aayush <aausgh@gmail.com>
2025-05-18 13:56:41 +0545

    mongodb connection

--- server/app.js
+++ server/app.js
@@ -1,12 +1,23 @@
 import express from 'express';
+import dotenv from 'dotenv';
+dotenv.config();
+import connectDB from "./src/config/db.config.js"
 
 const app = express();
 
+// Middleware to parse JSON bodies
+app.use(express.json());
+// Middleware to parse URL-encoded bodies
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-   res.send('Hello World!');
+   res.send('Welcome to the URL Shortener API');
 });
 
+
 const port = process.env.PORT || 3000;
+// Start the server
 app.listen(port, () => {
+   connectDB();
    console.log(`Server is running on port ${port}`);
 });
